b039917ed00780e05143f24a93360075a33b6aeb
Greg Misicko <gregmmail@gmail.com>
2013-10-27 01:02:59 -0400

    Added menu options to index

--- views/_v_template.php
+++ views/_v_template.php
@@ -11,6 +11,28 @@
 </head>
 
 <body>	
+	
+    <div id='menu'>
+
+        <a href='/'>Home</a>
+
+        <!-- Menu for users who are logged in -->
+        <?php if($user): ?>
+
+            <a href='/users/logout'>Logout</a>
+            <a href='/users/profile'>Profile</a>
+
+        <!-- Menu options for users who are not logged in -->
+        <?php else: ?>
+
+            <a href='/users/signup'>Sign up</a>
+            <a href='/users/login'>Log in</a>
+
+        <?php endif; ?>
+
+    </div>
+
+    <br>	
 
 	<?php if(isset($content)) echo $content; ?>
 
