a4e9914b400d03993a2c8f2c43eca2ed8d1a26a4
Greg Misicko <gregmmail@gmail.com>
2013-11-03 02:59:46 -0500

    More text size updates

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -1,4 +1,4 @@
-<?php echo $user->first_name; ?> - posts from people you are following:<br><br>
+<h1><?php echo $user->first_name; ?> - posts from people you are following:</h1><br><br>
 
 <?php $i = 0; ?>
 
