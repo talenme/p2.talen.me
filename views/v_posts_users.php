1c4b04ddb529a6ce21440c7d5a8077ca5139ebec
Greg Misicko <gregmmail@gmail.com>
2013-11-06 01:20:18 -0500

    Added profile info to user list

--- views/v_posts_users.php
+++ views/v_posts_users.php
@@ -3,20 +3,26 @@
 <?php if(isset($_GET["redirect"])) {echo 
     'You are currently not following any users - you may select one or more from the list below<br><br>';}?>
 
-<?php foreach($users as $user): ?>
+<table border=0>
+    <?php foreach($users as $user): ?>
+        <tr><td>
+            <!-- Print this user's name -->
+            <?=$user['first_name']?> <?=$user['last_name']?> 
+            <?php if(isset($user['location'])) { echo '('.$user['location'].')';}?>
 
-    <!-- Print this user's name -->
-    <?=$user['first_name']?> <?=$user['last_name']?>
+        <!-- If there exists a connection with this user, show an unfollow link -->
+        <?php if(isset($connections[$user['user_id']])): ?>
+            <a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
 
-    <!-- If there exists a connection with this user, show an unfollow link -->
-    <?php if(isset($connections[$user['user_id']])): ?>
-        <a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
+        <!-- Otherwise, show the follow link -->
+        <?php else: ?>
+            <a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
+        <?php endif; ?>
+        <br>
+        <?php if(isset($user['about'])) { echo 'About: '.$user['about'];}?>
 
-    <!-- Otherwise, show the follow link -->
-    <?php else: ?>
-        <a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
-    <?php endif; ?>
-
-    <br><br>
-
+        <br>
+        <hr>
+    </tr></td>
 <?php endforeach; ?>
+</table>
