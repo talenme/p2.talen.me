c9fe79cef73293c4fbbac075daea574e93c17455
Greg Misicko <gregmmail@gmail.com>
2013-11-06 10:28:55 -0500

    Updated info on login page

--- views/v_index_not.php
+++ views/v_index_not.php
@@ -5,10 +5,6 @@ You will need to <a href="/users/login">sign in</a> or <a href="/users/signup">c
 Additional edits to original code:<br>
 - error checking for the signup process (validate all fields have something entered, verify email address is valid format and unique)
 <br>
-- after signup is complete, send them to home page
-<br>
-- home page updated to show list of posts, option to add new posts, option to view available users to follow
-<br>
 - /posts/index numbered the entries, enable ascending/descending sorting, if zero posts recommend adding people to follow
 <br>
 - ability to add profile info (location, bio). I tried to get images to work, but couldn't.
